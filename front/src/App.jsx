bf47e5052f1c4aff7517bcd82b6b3e990c2a83a3
janparacha <parmusta@gmail.com>
2025-04-13 17:55:13 +0200

    React + Tailwind + Minimal NestJs + Docker (With HMR Working)

---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-red-500">
+      Application de chat pour le projet Nest JS
+    </div>
+  );
+}
+
+export default App;
